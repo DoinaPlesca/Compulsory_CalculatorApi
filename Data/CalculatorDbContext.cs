d38c4462453c253b2f27b1dcad11dcea9aa2d817
Doina Plesca <doinitzaplesca@yahoo.com>
2025-04-07 09:36:04 +0200

    add db context

--- Data/CalculatorDbContext.cs
+++ Data/CalculatorDbContext.cs
@@ -1,6 +1,12 @@
+using CalculatorApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace CalculatorApi.Data;
 
-public class CalculatorDbContext
+public class CalculatorDbContext : DbContext
 {
+    public CalculatorDbContext (DbContextOptions<CalculatorDbContext> options) 
+        : base(options) {}
     
+    public DbSet<CalculationRequest> Calculations { get; set; }
 }
