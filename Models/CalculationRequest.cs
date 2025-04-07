2ddba344572363dc7a4c9e975b7f82cd0ee5aea9
Doina Plesca <doinitzaplesca@yahoo.com>
2025-04-07 10:08:23 +0200

    create migrations; add model,setup program.cs

--- Models/CalculationRequest.cs
+++ Models/CalculationRequest.cs
@@ -2,5 +2,8 @@ namespace CalculatorApi.Models;
 
 public class CalculationRequest
 {
-    
+    public int Id { get; set; }
+    public string Expression { get; set; }
+    public double Result { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 }
